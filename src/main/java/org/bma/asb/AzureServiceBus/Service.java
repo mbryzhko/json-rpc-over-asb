66f4d8455c35f7128af26fed05512de5da19c1be
Maksym Brzhko <maxim.bryzhko@gmail.com>
2013-02-05 23:13:40 +0200

    init commit

---
+++ src/main/java/org/bma/asb/AzureServiceBus/Service.java
@@ -0,0 +1,63 @@
+package org.bma.asb.AzureServiceBus;
+
+import com.microsoft.windowsazure.services.core.Configuration;
+import com.microsoft.windowsazure.services.core.ServiceException;
+import com.microsoft.windowsazure.services.serviceBus.ServiceBusConfiguration;
+import com.microsoft.windowsazure.services.serviceBus.ServiceBusContract;
+import com.microsoft.windowsazure.services.serviceBus.ServiceBusService;
+import com.microsoft.windowsazure.services.serviceBus.models.BrokeredMessage;
+import com.microsoft.windowsazure.services.serviceBus.models.ReceiveMessageOptions;
+import com.microsoft.windowsazure.services.serviceBus.models.ReceiveMode;
+import com.microsoft.windowsazure.services.serviceBus.models.ReceiveQueueMessageResult;
+
+public class Service {
+	
+	public static void main(String[] args) throws ServiceException {
+		String issuer = "owner";
+		String key = "/YO6Q9o94KLmjp4Ce0jDwWsDnZ4Hfk7UOVnh1pP1KMc=";
+		Configuration config = ServiceBusConfiguration
+				.configureWithWrapAuthentication("makethingsbma", issuer, key,
+						".servicebus.windows.net/",
+						"-sb.accesscontrol.windows.net/WRAPv0.9");
+		System.out.println("Created Configuration " + config);
+		ServiceBusContract service = ServiceBusService.create(config);
+
+		System.out.println("Created Service Bus Service" + service);
+		
+		ReceiveMessageOptions opts = ReceiveMessageOptions.DEFAULT;
+		opts.setReceiveMode(ReceiveMode.RECEIVE_AND_DELETE);
+		while(true)
+		{ 
+		     ReceiveQueueMessageResult resultQM = 
+		        service.receiveQueueMessage("ideaservicequeue", opts);
+		     BrokeredMessage message = resultQM.getValue(); 
+		     if (message != null && message.getMessageId() != null)
+		     {
+		        try 
+		        {
+		           System.out.println("Body: " + message.getBody());
+		           System.out.println("MessageID: " + message.getMessageId());
+		           System.out.println("Custom Property: " + 
+		                message.getProperty("prop"));
+		           // Remove message from queue
+		           System.out.println("Deleting this message.");
+		           //service.deleteMessage(message);
+		        }
+		        catch (Exception ex)
+		        {
+		           // Indicate a problem, unlock message in queue
+		           System.err.println("Inner exception encountered!");
+		           ex.printStackTrace();
+		           //service.unlockMessage(message);
+		        }
+		     }
+		     else
+		     {
+		        System.out.println("Finishing up - no more messages.");
+		        break; 
+		        // Added to handle no more messages in the queue.
+		        // Could instead wait for more messages to be added.
+		     }
+		}
+	}
+}
