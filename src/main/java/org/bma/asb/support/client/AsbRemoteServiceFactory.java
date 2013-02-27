abf4465eb6fceb5d962fc8245f5a4b706696a376
mbryzhko <maxim.bryzhko@gmail.com>
2013-02-27 22:51:14 +0200

    misc changes

--- src/main/java/org/bma/asb/support/client/AsbRemoteServiceFactory.java
+++ src/main/java/org/bma/asb/support/client/AsbRemoteServiceFactory.java
@@ -4,7 +4,6 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
-import org.bma.asb.support.AsbServiceManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.FactoryBean;
--- src/main/java/org/bma/asb/support/client/DefaultAsbJsonRpcClient.java
+++ src/main/java/org/bma/asb/support/client/DefaultAsbJsonRpcClient.java
@@ -6,12 +6,16 @@ import java.io.OutputStream;
 import java.lang.reflect.Method;
 
 import org.bma.asb.support.AsbException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.googlecode.jsonrpc4j.JsonRpcClient;
 import com.googlecode.jsonrpc4j.ReflectionUtil;
 
 public class DefaultAsbJsonRpcClient implements AsbJsonRpcClient {
 
+	private final static Logger LOG = LoggerFactory.getLogger(DefaultAsbJsonRpcClient.class);
+	
 	private JsonRpcClient jsonRpcClient;
 	private boolean useNamedParams = true;
 
@@ -27,6 +31,7 @@ public class DefaultAsbJsonRpcClient implements AsbJsonRpcClient {
 		try {
 			jsonRpcClient.invoke(method.getName(), arguments, outputStream);
 		} catch (IOException e) {
+			LOG.error("Error serialization request {}", e);
 			throw new AsbException(e);
 		}
 	}
@@ -35,8 +40,10 @@ public class DefaultAsbJsonRpcClient implements AsbJsonRpcClient {
 		try {
 			return jsonRpcClient.readResponse(methodReturnType, responseIs);
 		} catch (AsbServiceException e) {
+			LOG.info("Received exception thrown by service method {}", e.getPayload().getMessage());
 			throw e.getPayload();
 		} catch (Throwable e) {
+			LOG.error("Error deserialization reponse {}", e);
 			throw new AsbException("Error read reponse", e);
 		}
 	}
